3fcc2f82dcabf350343fd97f05726f1fe6d3f7ad
Devan Parsley <djparsley@yahoo.com>
2020-06-01 17:20:31 -0400

    Fixed Issues with previous set of commits

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Input, Button, Form, Switch } from 'antd';
 import {postTodo} from "../services/todoService";
 
@@ -49,50 +48,4 @@ const TodoForm = (props: TodoFormInterface) => {
         </Form>
     );
 }
-=======
-import {Input, Button, Form, Switch} from 'antd';
-
-const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 }
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 }
-};
-
-const TodoForm = () => {
-        const [form] = Form.useForm();
-
-        const onFinish = (values: any) => {
-            console.log(values);
-        };
-
-        const onReset = () => {
-            form.resetFields();
-        };
-
-        return (
-            <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-                <Form.Item name="title" label="Title" rules={[{ required: true}]}>
-                    <Input />
-                </Form.Item>
-                <Form.Item name="descrpition" label="Description" rules={[{ required: true}]}>
-                    <Input.TextArea />
-                </Form.Item>
-                <Form.Item name="complete" label="Complete"  valuePropName="checked" initialValue={false}>
-                    <Switch />
-                </Form.Item>
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                    <Button htmlType="button" onClick={onReset}>
-                        Reset
-                    </Button>
-                </Form.Item>
-            </Form>
-        );
-}
-
->>>>>>> origin/dev
 export default TodoForm;
